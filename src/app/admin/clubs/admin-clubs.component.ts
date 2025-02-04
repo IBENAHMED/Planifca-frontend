44b451e9c50f68c85c6aeb7478cdf0bbeb332917
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2025-02-04 23:52:09 +0100

    refactor(admin): clubs

--- src/app/admin/clubs/admin-clubs.component.ts
+++ src/app/admin/clubs/admin-clubs.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
-import { TagButtonComponent } from "../../components/tag/tag-button/tag-button.component";
+import { AdminLayoutComponent } from '../layout/admin-layout.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [TagButtonComponent],
+  imports: [AdminLayoutComponent],
   templateUrl: './admin-clubs.component.html',
   styleUrl: './admin-clubs.component.scss'
 })
 export class AdminClubsComponent {
-  mesClubs(): void {
-    alert("Fonctionnalités en cours");
-  };
 
-  createClub(): void {
-    alert("Fonctionnalités en cours");
-  };
-};
+}
