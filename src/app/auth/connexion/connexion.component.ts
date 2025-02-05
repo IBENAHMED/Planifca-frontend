95e74d419eea597d8e6c28eb3a1b5603901f8205
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2025-02-05 02:13:52 +0100

    refactor(admin): router

--- src/app/auth/connexion/connexion.component.ts
+++ src/app/auth/connexion/connexion.component.ts
@@ -69,7 +69,8 @@ export class ConnexionComponent implements OnInit {
     const credentials: connexion = {
       email: this.connexionForm.get('email')?.value,
       password: this.connexionForm.get('password')?.value,
-      // # todo create middleweare
+      // # todo already you craeted layout now you need to create middleweare
+      // todo: avaner dans espace club (ceconnection adn design)
       // userType: this.userType, #todo active type when get ready on backend
     };
 
