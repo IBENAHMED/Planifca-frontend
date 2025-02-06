e4fec4c19a54904b66db450b5a696619a362afc5
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2025-02-06 22:41:16 +0100

    refactor(admin): admin

--- src/app/admin/layout/admin-layout.component.ts
+++ src/app/admin/layout/admin-layout.component.ts
@@ -1,37 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { URLS } from '../../components/helpers/url-constants';
-import { TagButtonComponent } from '../../components/tag/tag-button/tag-button.component';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [TagButtonComponent, RouterLink],
+  imports: [NgIf, RouterLink],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
 export class AdminLayoutComponent {
+  isOpen = false;
 
-  isOffcanvasOpen = false;
-  urlAdminClubs = URLS.ADMIN_CLUB;
-
-  openOffcanvas() {
-    this.isOffcanvasOpen = true;
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
   }
-
-  closeOffcanvas() {
-    this.isOffcanvasOpen = false;
-  }
-
-  logOut(): void {
-    alert("Fonctionnalités en cours");
-  };
-
-  mesClubs(): void {
-    alert("Fonctionnalités en cours");
-  };
-
-  createClub(): void {
-    alert("Fonctionnalités en cours");
-  };
 }
