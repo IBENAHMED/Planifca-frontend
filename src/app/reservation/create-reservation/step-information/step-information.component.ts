eed0049dd79500602e0dceed6f881addd2de8538
MohamedAtyq <mohamedatyq422@gmail.com>
2025-02-23 23:16:41 +0100

    add component for reservation

---
+++ src/app/reservation/create-reservation/step-information/step-information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './step-information.component.html',
+  styleUrl: './step-information.component.scss'
+})
+export class StepInformationComponent {
+
+}
